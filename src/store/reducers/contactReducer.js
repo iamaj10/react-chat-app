ed8e2d9a6064046e7c553901269a6ef900956c34
angaj <angaj@enfono.in>
2024-01-10 20:15:28 +0530

    added success messages

--- src/store/reducers/contactReducer.js
+++ src/store/reducers/contactReducer.js
@@ -4,9 +4,6 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 //GETTING TOAST
 import { toast } from "react-toastify";
 
-//GETTING ACTION FROM CART REDUCER
-// import { setCart } from "./cartReducer";
-
 //GETTING COMPONENTS FROM FIREBASE FILE
 import { db } from "../../firebaseinit";
 
@@ -24,58 +21,6 @@ const INITIAL_STATE = {
   contacts: [],
 };
 
-//purchase
-// export const purchase = createAsyncThunk(
-//   "orders/purchase",
-//   async ({ carts, orders }, thunkAPI) => {
-//     //setting date for order
-//     const currentDate = new Date();
-//     const year = currentDate.getFullYear();
-//     const month = currentDate.getMonth() + 1;
-//     const date = currentDate.getDate();
-
-//     const orderDate = `${date.toString()}/${month.toString()}/${year.toString()}`;
-
-//     //make order in object
-//     const newOrder = { date: orderDate, order: carts };
-//     //set order in order array
-//     thunkAPI.dispatch(checkOut(newOrder));
-//     toast.success("Item Purchased");
-
-//     //set updated order to database also
-//     const updatedOrders = [...orders, newOrder];
-//     const users = collection(db, "users");
-//     const querySnapshot = await getDocs(users);
-//     querySnapshot.forEach(async (user) => {
-//       const userId = user.id;
-//       const useRef = doc(db, "users", userId);
-//       await updateDoc(useRef, {
-//         orders: updatedOrders,
-//         carts: [],
-//       });
-//     });
-
-//     //set cart empty after order purchased
-//     thunkAPI.dispatch(setCart([]));
-//   }
-// );
-
-// export const fetchContacts = createAsyncThunk(
-//   "chats/fetch",
-//   async ({ contactID }, thunkAPI) => {
-//     try {
-//       const querySnapshot = await getDocs(collection(db, "contacts"));
-//       querySnapshot.forEach((doc) => {
-//         // doc.data() is never undefined for query doc snapshots
-//         // console.log(doc.id, " => ", doc.data());
-//         thunkAPI.dispatch(getChat(doc.data()));
-//       });
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   }
-// );
-
 export const fetchContacts = createAsyncThunk(
   "contacts/fetch",
   async (_, thunkAPI) => {
@@ -88,8 +33,10 @@ export const fetchContacts = createAsyncThunk(
           thunkAPI.dispatch(getChat(doc.data()));
         });
       });
+      toast.success("Contacts Loaded");
     } catch (e) {
       console.log(e);
+      toast.error("Failed to Load Contacts. Try Again");
     }
   }
 );
@@ -121,12 +68,6 @@ const contactsSlice = createSlice({
   name: "chats",
   initialState: INITIAL_STATE,
   reducers: {
-    // setOrders: (state, action) => {
-    //   state.orders = action.payload;
-    // },
-    // checkOut: (state, action) => {
-    //   state.orders.push(action.payload);
-    // },
     getChat: (state, action) => {
       const newContact = action.payload;
 
